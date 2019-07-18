1c6f96c0ae2955577e1ea11631df3adaaf60be56
akitook <akito.okubo@gmail.com>
2019-07-19 08:28:20 +0900

    fix assets

--- pages/threejs/arjs01.vue
+++ pages/threejs/arjs01.vue
@@ -3,9 +3,9 @@
     <InfoPanel :article="article" />
     <div class="container">
       <div class="one">
-        <h2>1. Scan QR code to open web app in your phone browser.</h2>
+        <h2>1. Scan this QR code to open web app in your phone browser.</h2>
         <h2>2. Scan this marker image.</h2>
-        <img src="~/assets/sketch/arjs01/assets/qrmarker.png" alt="QR CODE" />
+        <img src="~/assets/sketch/arjs01/assets/qrmarker.jpg" alt="QR CODE" />
       </div>
     </div>
   </div>
@@ -28,13 +28,33 @@ export default {
 </script>
 
 <style scoped lang="scss">
+h2 {
+  font-size: 14px;
+}
 .container {
   width: 100vw;
   height: 100vh;
+  padding: 32px;
   display: flex;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
+}
+.one {
+  max-width: 100%;
+  img {
+    max-width: 100%;
+  }
 }
 @media screen and (min-width: 768px) {
+  .container {
+    align-items: center;
+  }
+  .one {
+    max-width: 512px;
+  }
+  h2 {
+    text-align: center;
+    margin-bottom: 16px;
+  }
 }
 </style>
